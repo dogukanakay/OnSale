f9bb6098fc7b44648676d1b6b78136c32c72666d
dogukanakay <akay.dogukann@gmail.com>
2024-08-23 23:47:37 +0300

    EventBus Base and Azure Service Bus Added

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+
+        public Guid Id { get; private set; }
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
+
+       
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(DateTime createdDate, Guid id)
+        {
+            CreatedDate = createdDate;
+            Id = id;
+        }
+    }
+}
