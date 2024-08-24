e0f79cdfbbe2ef55daf6576326e2b7f323d4c33b
dogukanakay <akay.dogukann@gmail.com>
2024-08-24 23:27:31 +0300

    Unit Tests for Azure Service Bus and RabbitMQ

---
+++ tests/BuildingBlocks/EventBus.UnitTest/EventBusTest.cs
@@ -0,0 +1,124 @@
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Factory;
+using EventBus.UnitTest.Events.EventHandlers;
+using EventBus.UnitTest.Events.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace EventBus.UnitTest
+{
+    [TestClass]
+    public class EventBusTest
+    {
+        private ServiceCollection services;
+
+        public EventBusTest()
+        {
+            services = new ServiceCollection();
+            services.AddLogging(configure => configure.AddConsole());
+
+        }
+
+        [TestMethod]
+        public void Subscribe_Event_On_RabbitMQ_Test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+               
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+            //eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+
+        }
+
+        [TestMethod]
+        public void Subscribe_Event_On_Azure_Test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+                return EventBusFactory.Create(GetAzureConfig(), sp);
+            });
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Subscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+            //eventBus.UnSubscribe<OrderCreatedIntegrationEvent, OrderCreatedIntegrationEventHandler>();
+            Task.Delay(2000).Wait();
+
+        }
+
+
+        [TestMethod]
+        public void Send_Message_To_RabbitMQ_Test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+
+                return EventBusFactory.Create(GetRabbitMQConfig(), sp);
+            });
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Publish(new OrderCreatedIntegrationEvent(2));
+        }
+        
+        [TestMethod]
+        public void Send_Message_To_Azure_Test()
+        {
+            services.AddSingleton<IEventBus>(sp =>
+            {
+
+                return EventBusFactory.Create(GetAzureConfig(), sp);
+            });
+            var sp = services.BuildServiceProvider();
+
+            var eventBus = sp.GetRequiredService<IEventBus>();
+
+            eventBus.Publish(new OrderCreatedIntegrationEvent(2));
+        }
+
+
+        
+        private EventBusConfig GetAzureConfig()
+        {
+            return new EventBusConfig
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "OnSaleTopicName",
+                EventBusType = EventBusType.AzureServiceBus,
+                EventNameSuffix = "IntegrationEvent",
+                EventBusConnectionString = "Endpoint=sb://onsell.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h/Eae4YrSFjPSIiRnEe1rKRwZW9LO4dTZ+ASbDLjbkU="
+
+            };
+        }
+        
+        private EventBusConfig GetRabbitMQConfig()
+        {
+            return new EventBusConfig
+            {
+                ConnectionRetryCount = 5,
+                SubscriberClientAppName = "EventBus.UnitTest",
+                DefaultTopicName = "OnSaleTopicName",
+                EventBusType = EventBusType.RabbitMQ,
+                EventNameSuffix = "IntegrationEvent"
+                //Connection = new ConnectionFactory() // Zaten default olarak bu ayarlar tanımlı eğer rabbitmq seçilmiş ise.
+                //{
+                //    HostName = "localhost",
+                //    Port = 5672,
+                //    UserName = "guest",
+                //    Password = "guest"
+                //}
+            };
+        }
+    }
+}
