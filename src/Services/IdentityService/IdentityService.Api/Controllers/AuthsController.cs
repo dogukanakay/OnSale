13c99bee33f1c2810db06f766dd21013cb735ec3
dogukanakay <akay.dogukann@gmail.com>
2024-08-26 18:14:54 +0300

    Basic IdentityService Added

---
+++ src/Services/IdentityService/IdentityService.Api/Controllers/AuthsController.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer.Application.Models;
+using IdentityServer.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+
+        public AuthsController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequestModel loginRequestModel)
+        {
+            var result = await _identityService.Login(loginRequestModel);
+            return Ok(result);
+        }
+    }
+}
